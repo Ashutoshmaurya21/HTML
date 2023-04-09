a205bfd8cf70b7746bb98b69350b70bf101527eb
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2023-04-09 22:48:27 +0530

    Create HTML.h

---
+++ HTML.h
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> the greater  coder </title>
+    </head>
+    
+<body>
+    <h1>
+        hello world 
+    </h1>
+    <h2>
+        this is my first code in html 
+    </h2>
+</body>
+</html>
